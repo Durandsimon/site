d67f9467718a72d611c7e3b11089232d1763eb74
Simon Durand <91254616+Durandsimon@users.noreply.github.com>
2022-12-20 15:44:32 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,28 @@
+
+
+
+intervalle = setInterval(traitnoir1, 500);
+intervalle = setInterval(traitnoir2, 600);
+intervalle = setInterval(traitnoir3, 700);
+intervalle = setInterval(traitnoir4, 800);
+intervalle = setInterval(traitnoir5, 900);
+
+function traitnoir1() {
+    document.querySelector('#trait1').style.top = '14vw';
+}
+
+function traitnoir2() {
+    document.querySelector('#trait2').style.top = '14vw';
+}
+
+function traitnoir3() {
+    document.querySelector('#trait3').style.top = '14vw';
+}
+
+function traitnoir4() {
+    document.querySelector('#trait4').style.top = '14vw';
+}
+
+function traitnoir5() {
+    document.querySelector('#trait5').style.top = '14vw';
+}
