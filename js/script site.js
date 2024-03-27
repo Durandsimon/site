db99dc045d3e9ddf5b53755033434e77434f7b8a
Simon Durand <91254616+Durandsimon@users.noreply.github.com>
2024-03-27 22:32:07 +0100

    Add files via upload

---
+++ js/script site.js
@@ -0,0 +1,34 @@
+
+
+document.querySelector("#zone1").addEventListener("click", depliant);
+document.querySelector("#zone2").addEventListener("click", depliant);
+document.querySelector("#zone3").addEventListener("click", depliant);
+document.querySelector("#zone4").addEventListener("click", depliant);
+
+
+let click_text_video = 0;
+
+let num = 0;
+
+
+function depliant(){
+    if(num != this.id.substr(4, 1)){
+        click_text_video = 0;
+    }
+    
+    num = this.id.substr(4, 1);
+    if (click_text_video == 0) {
+        document.querySelector("#text-video-"+num).style.display = 'block'
+        document.querySelector("#fleche"+num).style.transform = 'rotate(180deg)'
+        document.querySelector("#zone"+num).style.padding = '5% 5% 3% 5% '
+        
+        click_text_video = 1;
+    }
+    else {
+        document.querySelector("#text-video-"+num).style.display = 'none'
+        document.querySelector("#fleche"+num).style.transform = 'rotate(0deg)'
+        document.querySelector("#zone"+num).style.padding = '5% 5% 5% 5% '
+        click_text_video = 0;
+    }
+    
+}
