e3bb48e6731c2614362f9bc93e07b725c5485910
Simon Durand <91254616+Durandsimon@users.noreply.github.com>
2024-04-03 20:26:42 +0200

    Add files via upload

---
+++ js/script code.js
@@ -0,0 +1,32 @@
+
+
+document.querySelector("#zone1").addEventListener("click", depliant);
+
+
+
+let click_text_video = 0;
+
+let num = 0;
+
+
+function depliant(){
+    if(num != this.id.substr(4, 1)){
+        click_text_video = 0;
+    }
+    
+    num = this.id.substr(4, 1);
+    if (click_text_video == 0) {
+        document.querySelector("#text-video-"+num).style.display = 'block'
+        document.querySelector("#fleche"+num).style.transform = 'rotate(180deg)'
+        document.querySelector("#zone"+num).style.padding = '5% 5% 3% 5% '
+        
+        click_text_video = 1;
+    }
+    else {
+        document.querySelector("#text-video-"+num).style.display = 'none'
+        document.querySelector("#fleche"+num).style.transform = 'rotate(0deg)'
+        document.querySelector("#zone"+num).style.padding = '5% 5% 5% 5% '
+        click_text_video = 0;
+    }
+    
+}
---
+++ js/script index.js
@@ -0,0 +1,29 @@
+
+
+
+intervalle = setInterval(traitnoir1, 300);
+intervalle = setInterval(traitnoir2, 400);
+intervalle = setInterval(traitnoir3, 500);
+intervalle = setInterval(traitnoir4, 600);
+intervalle = setInterval(traitnoir5, 700);
+
+function traitnoir1() {
+    document.querySelector('#trait1').style.top = '14vw';
+}
+
+function traitnoir2() {
+    document.querySelector('#trait2').style.top = '14vw';
+}
+
+function traitnoir3() {
+    document.querySelector('#trait3').style.top = '14vw';
+}
+
+function traitnoir4() {
+    document.querySelector('#trait4').style.top = '14vw';
+}
+
+function traitnoir5() {
+    document.querySelector('#trait5').style.top = '14vw';
+}
+
